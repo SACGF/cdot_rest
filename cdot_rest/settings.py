888b43637772e56395513357a9d7f5fd811681d2
Dave Lawrence <davmlaw@gmail.com>
2025-07-29 16:14:18 +0930

    New cdot.cc IP

--- cdot_rest/settings.py
+++ cdot_rest/settings.py
@@ -22,7 +22,7 @@ SECRET_KEY = get_or_create_django_secret_key(THIS_DIR)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["cdot.cc", "203.101.229.44", "localhost"]
+ALLOWED_HOSTS = ["cdot.cc", "129.127.18.77", "localhost"]
 
 
 # Application definition
